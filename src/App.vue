a6fa0c533fc22b2f772cfa64d551b3177f0d0de7
samyfodil <fodil.samy@gmail.com>
2023-06-16 19:52:10 -0500

    good to go

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" style="max-width: 500px;">
+  <HelloWorld msg="A Cloud Computing Network where everything is a CID!"/>
 </template>
 
 <script>
